3dbe3938b5116dc345195b47145921450f2ace93
tswu <tswu@umich.edu>
2025-03-02 17:47:58 -0500

    server created

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,5 @@
 module github.com/tongshengw/nbody-game
 
 go 1.23.6
+
+require github.com/gorilla/websocket v1.5.3 // indirect
--- server/main.go
+++ server/main.go
@@ -1,8 +1,19 @@
 package main
 
-import ()
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+)
 
 func main() {
+	nbody_ascii := `
+      ___.              .___      
+  ____\_ |__   ____   __| _/__.__.
+ /    \| __ \ /  _ \ / __ <   |  |
+|   |  \ \_\ (  <_> ) /_/ |\___  |
+|___|  /___  /\____/\____ |/ ____|
+     \/    \/            \/\/
+	`
 
+	fmt.Printf("%s\nStarting nbody-server...\n", nbody_ascii)
 }
-
