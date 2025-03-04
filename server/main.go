d89f9a07fd568fac7882f5da8907f4ae47e8ae78
tswu <tswu@umich.edu>
2025-03-03 20:15:45 -0500

    boilerplate websocket

--- server/main.go
+++ server/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/gorilla/websocket"
+	"net/http"
 )
 
 func main() {
@@ -16,33 +18,31 @@ func main() {
 	`
 
 	fmt.Printf("%s\nStarting nbody-server...\n", nbody_ascii)
-	
+
 	var addr = flag.String("addr", ":8080", "http service address")
-	
-	func serveHome(w http.ResponseWriter, r *http.Request) {
-		log.Println(r.URL)
-		if r.URL.Path != "/" {
-			http.Error(w, "Not found", http.StatusNotFound)
-			return
-		}
-		if r.Method != http.MethodGet {
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-			return
-		}
-		http.ServeFile(w, r, "home.html")
+
+	flag.Parse()
+	hub := newHub()
+	go hub.run()
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
 	}
-	
-	func main() {
-		flag.Parse()
-		hub := newHub()
-		go hub.run()
-		http.HandleFunc("/", serveHome)
-		http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-			serveWs(hub, w, r)
-		})
-		err := http.ListenAndServe(*addr, nil)
-		if err != nil {
-			log.Fatal("ListenAndServe: ", err)
-		}
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
 	}
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "home.html")
 }
