240e337cc65819d79ac073e96ed965e06ee453fb
tongshengw <trinityrushworth@gmail.com>
2025-03-03 21:49:29 -0500

    working websockets

--- game/main.gd
+++ game/main.gd
@@ -1,15 +1,30 @@
 extends Node3D
 
+var connected := false
+var counter := 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("mainready")
-	WebsocketClient.connect_to_url("localhost:9999/ws")
-	WebsocketClient.send("diddy")
-	print("msgsent")
+	WebsocketClient.connected_to_server.connect(connectHandler)
+	WebsocketClient.connection_closed.connect(disconnectHandler)
+	WebsocketClient.message_received.connect(msgHandler)
+
+	print("here")
+	WebsocketClient.connect_to_url("ws://127.0.0.1:9999/ws")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	WebsocketClient.send("loop")
-	print("loop")
+	if connected:
+		WebsocketClient.send(str(counter))
+		counter += 1
+
+func connectHandler() -> void:
+	connected = true
+	WebsocketClient.send("connected")
+	print("connected")
+func disconnectHandler() -> void:
+	print("disconnected")
+func msgHandler(message: Variant) -> void:
+	print(message)
